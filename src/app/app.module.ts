c84c3369837c96f9d87e12905125d6d126f5407b
Alvin-GitHub <i_dever@hotmail.com>
2018-04-27 10:31:58 +1200

    updata hoek ~> 5.0.3. in package-lock.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 
-//这个根模块会告诉 Angular 如何组装该应用
+// 这个根模块会告诉 Angular 如何组装该应用
 
 
-//引入模块
+// 引入模块
 
-import { BrowserModule } from '@angular/platform-browser'; //*BrowserModule，浏览器解析的模块*/
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';  /*核心模块*/
-import { HttpModule }    from '@angular/http';
+import { HttpModule } from '@angular/http';
 
-import {AppRoutingModule} from './routers/app.routing.module';
+import { AppRoutingModule } from './routers/app.routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -16,10 +16,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { Page1Component } from './components/page1/page1.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { ContactsComponent} from './components/contacts/contacts.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { JobsComponent } from './components/jobs/jobs.component';
- /*自定义的模块*/
+/*自定义的模块*/
 
 
 /*@NgModule装饰器将AppModule标记为 Angular 模块类（也叫NgModule类）。
@@ -27,11 +27,11 @@ import { JobsComponent } from './components/jobs/jobs.component';
 
 @NgModule({
   declarations: [    /*引入当, JobsComponent前项目运行的�, DashboardComponent�组件  自定义组件都需要引入并且在这个里面配制*/
-    AppComponent, HeaderComponent, FooterComponent, Page1Component, SidebarComponent, BreadcrumbsComponent, 
-    ContactsComponent, JobsComponent,DashboardComponent
+    AppComponent, HeaderComponent, FooterComponent, Page1Component, SidebarComponent, BreadcrumbsComponent,
+    ContactsComponent, JobsComponent, DashboardComponent
   ],
   imports: [  /*当前的项目依赖哪些模块*/
-    BrowserModule,HttpModule,AppRoutingModule
+    BrowserModule, HttpModule, AppRoutingModule
   ],
   providers: [], /*定义的服务  回头放在这个里面*/
   bootstrap: [AppComponent]   /*默认启动那个组件*/
